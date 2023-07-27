75c199037fa14f08f44538ff7ac662d5c520a7ad
TheOnlyHiba <137408739+TheOnlyHiba@users.noreply.github.com>
2023-07-27 18:51:20 +0200

    Add files via upload
    this is the final project for opus lab

---
+++ PROJET/main2.js
@@ -0,0 +1,28 @@
+// hedhi el pop up mtaa el bronz
+
+let popup1 = document.getElementById("popup1");
+function openPopup(){
+  popup1.classList.add("open-popup");
+}
+function closePopup(){
+  popup1.classList.remove("open-popup");
+}
+// hedhi el pop up mtaa el silver
+
+let popup2 = document.getElementById("popup2");
+function showPup(){
+    popup2.classList.add("open-popup2")
+}
+function closePup(){
+    popup2.classList.remove("open-popup2")
+}
+
+// hedhi pop up mtaa el golden
+
+let popup3 = document.getElementById("popup3");
+function opnPup(){
+    popup3.classList.add("open-popup3")
+}
+function clsPup(){
+    popup3.classList.remove("open-popup3")
+}
